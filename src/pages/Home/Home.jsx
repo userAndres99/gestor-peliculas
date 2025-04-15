59720a739b19bebbe649ea21e31e5ebb5c5b17dd
Menard Andres <andresmenard.arg99@gmail.com>
2025-04-15 12:39:41 -0300

    agrego el home donde vamos a trabajar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.module.css'; 
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Bienvenidos al Gestor de Películas y Series</h1>
+      <p>Esta es la página principal del proyecto.</p>
+    </div>
+  );
+}
+
+export default Home;
